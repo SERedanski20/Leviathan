64d5e9c13a6fd2006d98517753042fdd5679e187
SERedanski20 <85338763+SERedanski20@users.noreply.github.com>
2022-10-25 22:14:17 +0300

    The Start of the Game

---
+++ LeviathanSRoyalty/Intermediate/Build/Win64/UnrealEditor/Inc/LeviathanSRoyalty/LeviathanSRoyaltyGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef LEVIATHANSROYALTY_LeviathanSRoyaltyGameModeBase_generated_h
+#error "LeviathanSRoyaltyGameModeBase.generated.h already included, missing '#pragma once' in LeviathanSRoyaltyGameModeBase.h"
+#endif
+#define LEVIATHANSROYALTY_LeviathanSRoyaltyGameModeBase_generated_h
+
+#define FID_LeviathanSRoyalty_Source_LeviathanSRoyalty_LeviathanSRoyaltyGameModeBase_h_15_SPARSE_DATA
+#define FID_LeviathanSRoyalty_Source_LeviathanSRoyalty_LeviathanSRoyaltyGameModeBase_h_15_RPC_WRAPPERS
+#define FID_LeviathanSRoyalty_Source_LeviathanSRoyalty_LeviathanSRoyaltyGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_LeviathanSRoyalty_Source_LeviathanSRoyalty_LeviathanSRoyaltyGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesALeviathanSRoyaltyGameModeBase(); \
+	friend struct Z_Construct_UClass_ALeviathanSRoyaltyGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ALeviathanSRoyaltyGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/LeviathanSRoyalty"), NO_API) \
+	DECLARE_SERIALIZER(ALeviathanSRoyaltyGameModeBase)
+
+
+#define FID_LeviathanSRoyalty_Source_LeviathanSRoyalty_LeviathanSRoyaltyGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesALeviathanSRoyaltyGameModeBase(); \
+	friend struct Z_Construct_UClass_ALeviathanSRoyaltyGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ALeviathanSRoyaltyGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/LeviathanSRoyalty"), NO_API) \
+	DECLARE_SERIALIZER(ALeviathanSRoyaltyGameModeBase)
+
+
+#define FID_LeviathanSRoyalty_Source_LeviathanSRoyalty_LeviathanSRoyaltyGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ALeviathanSRoyaltyGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ALeviathanSRoyaltyGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ALeviathanSRoyaltyGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ALeviathanSRoyaltyGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ALeviathanSRoyaltyGameModeBase(ALeviathanSRoyaltyGameModeBase&&); \
+	NO_API ALeviathanSRoyaltyGameModeBase(const ALeviathanSRoyaltyGameModeBase&); \
+public:
+
+
+#define FID_LeviathanSRoyalty_Source_LeviathanSRoyalty_LeviathanSRoyaltyGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ALeviathanSRoyaltyGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ALeviathanSRoyaltyGameModeBase(ALeviathanSRoyaltyGameModeBase&&); \
+	NO_API ALeviathanSRoyaltyGameModeBase(const ALeviathanSRoyaltyGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ALeviathanSRoyaltyGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ALeviathanSRoyaltyGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ALeviathanSRoyaltyGameModeBase)
+
+
+#define FID_LeviathanSRoyalty_Source_LeviathanSRoyalty_LeviathanSRoyaltyGameModeBase_h_12_PROLOG
+#define FID_LeviathanSRoyalty_Source_LeviathanSRoyalty_LeviathanSRoyaltyGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_LeviathanSRoyalty_Source_LeviathanSRoyalty_LeviathanSRoyaltyGameModeBase_h_15_SPARSE_DATA \
+	FID_LeviathanSRoyalty_Source_LeviathanSRoyalty_LeviathanSRoyaltyGameModeBase_h_15_RPC_WRAPPERS \
+	FID_LeviathanSRoyalty_Source_LeviathanSRoyalty_LeviathanSRoyaltyGameModeBase_h_15_INCLASS \
+	FID_LeviathanSRoyalty_Source_LeviathanSRoyalty_LeviathanSRoyaltyGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_LeviathanSRoyalty_Source_LeviathanSRoyalty_LeviathanSRoyaltyGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_LeviathanSRoyalty_Source_LeviathanSRoyalty_LeviathanSRoyaltyGameModeBase_h_15_SPARSE_DATA \
+	FID_LeviathanSRoyalty_Source_LeviathanSRoyalty_LeviathanSRoyaltyGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_LeviathanSRoyalty_Source_LeviathanSRoyalty_LeviathanSRoyaltyGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	FID_LeviathanSRoyalty_Source_LeviathanSRoyalty_LeviathanSRoyaltyGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> LEVIATHANSROYALTY_API UClass* StaticClass<class ALeviathanSRoyaltyGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_LeviathanSRoyalty_Source_LeviathanSRoyalty_LeviathanSRoyaltyGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
